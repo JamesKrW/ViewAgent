deb97b7ba1161b74e4771f69c4eb7d6b56e265fd
Kangrui Wang <kangruiwang.cs@gmail.com>
2026-09-05 22:40:21 -0700

    Add 512px ViewSuite and Habitat-GS configs

--- GraphRL/examples/viewsuite/habitat_gs_interactive_view_planning/run_adaptive.sh
+++ GraphRL/examples/viewsuite/habitat_gs_interactive_view_planning/run_adaptive.sh
@@ -6,6 +6,11 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 GRAPHRL_ROOT="$(cd "${SCRIPT_DIR}/../../.." && pwd)"
 export PYTHONPATH="${VIEWSUITE_ROOT}:${GRAPHRL_ROOT}:${GRAPHRL_ROOT}/VAGEN:${GRAPHRL_ROOT}/VAGEN/verl:${GRAPHRL_ROOT}/LLaMA-Factory/src${PYTHONPATH:+:${PYTHONPATH}}"
 
+TRAIN_CONFIG="${HABITAT_GS_TRAIN_CONFIG:-train.yaml}"
+VAL_CONFIG="${HABITAT_GS_VAL_CONFIG:-val.yaml}"
+[[ "${TRAIN_CONFIG}" = /* ]] || TRAIN_CONFIG="${SCRIPT_DIR}/${TRAIN_CONFIG}"
+[[ "${VAL_CONFIG}" = /* ]] || VAL_CONFIG="${SCRIPT_DIR}/${VAL_CONFIG}"
+
 EXPERIMENT_DIR="${PWD}/exps/viewagent/habitat_gs_ivp_relative_earlystop"
 N_GPUS_PER_NODE="${N_GPUS_PER_NODE:-8}"
 SFT_N_GPUS="${SFT_N_GPUS:-${N_GPUS_PER_NODE}}"
@@ -17,8 +22,8 @@ export WANDB_MODE="${WANDB_MODE:-online}"
 python -m graphrl.main_adaptive \
     --config-path="${SCRIPT_DIR}" \
     --config-name=pipeline_adaptive \
-    general_overrides.rl.hydra_overrides.data.train_files="${SCRIPT_DIR}/train.yaml" \
-    general_overrides.rl.hydra_overrides.data.val_files="${SCRIPT_DIR}/val.yaml" \
+    general_overrides.rl.hydra_overrides.data.train_files="${TRAIN_CONFIG}" \
+    general_overrides.rl.hydra_overrides.data.val_files="${VAL_CONFIG}" \
     general_overrides.rl.hydra_overrides.trainer.n_gpus_per_node="${N_GPUS_PER_NODE}" \
     general_overrides.rl.hydra_overrides.trainer.nnodes=1 \
     general_overrides.sft.n_gpus="${SFT_N_GPUS}" \
